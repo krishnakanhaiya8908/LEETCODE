900372c6574164a6b6751caa840ed3b53578bd76
Krishna Kanhaiya <119802765+krishnakanhaiya8908@users.noreply.github.com>
2026-05-23 02:37:27 +0530

    Time: 538 ms (5.24%) | Memory: 46.9 MB (63.93%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        //int k=0;
+        
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count!=2){
+                return nums[i];
+                }
+        }
+        return -1;
+        
+    }
+}
